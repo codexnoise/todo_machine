5b0a981504ba2a4c9907fc9aad78cc88fdd2c406
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-03-05 00:13:57 -0500

    add composition to TodoCounter and TodoSearch

---
+++ src/TodoHeader/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+
+export { TodoHeader };
