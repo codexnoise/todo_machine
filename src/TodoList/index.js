fba6ebd862db7decdd86177859d27c6eb051e342
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-04-04 21:39:21 -0500

    add conditions to render the TodoList-container

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -4,14 +4,14 @@ import "./TodoList.css";
 function TodoList(props) {
   const renderFunc = props.children || props.render;
   return (
-    <section class="TodoList-container">
+    <section className="TodoList-container">
       {props.error && props.onError()}
       {props.loading && props.onLoading()}
       {!props.loading && !props.totalTodos && props.onEmptyTodos()}
       {props.totalTodos &&
         !props.searchedTodos.length &&
         props.onEmptySearchResults(props.searchText)}
-      {props.searchedTodos.map(renderFunc)}
+      {!props.loading && !props.error && props.searchedTodos.map(renderFunc)}
     </section>
   );
 }
